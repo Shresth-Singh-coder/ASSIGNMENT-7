6dcdb6c920e0c03f708170aca6ddd650a433837f
Shresth-Singh-coder <shresthengineer@gmail.com>
2025-04-25 00:47:29 +0530

    Create Question 2.c

---
+++ Question 2.c
@@ -0,0 +1,59 @@
+//write a program to find height of binary tree.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* createNode(int value) {
+    struct node* newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct node* insert(struct node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+int height(struct node* root) {
+    if (root == NULL) {
+        return -1;
+    }
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+int main() {
+    struct node* root = NULL;
+    int n, value;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter %d node values:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    printf("\nHeight of the tree: %d\n", height(root));
+
+    return 0;
+}
